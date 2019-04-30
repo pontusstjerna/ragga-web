336443d53f3421dd099b32e46d1eb0bf0d82f968
Pontus Stjernström <pontus@wmyl.se>
2019-04-30 17:08:50 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { generateSE } from 'autoflirt';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            flirt: '',
+            isSerious: true,
+            isMean: false,
+        };
+
+        this.generate = this.generate.bind(this);
+    }
+
+    generate() {
+        const { isSerious, isMean } = this.state;
+        this.setState({flirt: generateSE(isSerious, isMean)});
+    }
+
+    render() {
+        const { flirt, isSerious, isMean } = this.state;
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>
+                        Välkommen till <b>Pontus Raggningsreplikgenerator</b>! <br />Tryck på knappen nedan för att generera en
+                        raggningsreplik.
+                    </p>
+                </header>
+                <body>
+                    <p className={"App-flirt" + flirt ? '' : 'App-flirt-hidden'}>{flirt ? flirt : 'aaa'}</p>
+                    <button className="btn-generate" onClick={this.generate}>Generera</button>
+
+                    <div className="app-settings">
+                        <input type="checkbox" checked={!isSerious} onClick={() => this.setState({isSerious: !isSerious})} />
+                        <label>Konstigt</label>
+                        <input type="checkbox" checked={isMean} onClick={() => this.setState({isMean: !isMean})} />
+                        <label>Elakt</label>
+                    </div>
+                </body>
+            </div>
+        );
+    }
 }
 
 export default App;
